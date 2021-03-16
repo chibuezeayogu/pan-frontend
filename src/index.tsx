0049424c57c848dc0eb062eb52052e47e6f63b49
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2021-03-16 03:23:04 +0100

    Added and configured webpack babel eslint

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import {render } from "react-dom";
+
+const ROOT = document.getElementById('root');
+
+render(<h1>Hello</h1>, ROOT);
---
+++ webpack.config.ts
@@ -0,0 +1,48 @@
+import path from "path";
+import webpack from "webpack";
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+
+const config: webpack.Configuration = {
+  entry: "./src/index.tsx",
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins:[
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      eslint: {
+        files: ".src/**/*",
+      }
+    })
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    compress: true,
+    port: 4000,
+  },
+}
+
+export default config;
