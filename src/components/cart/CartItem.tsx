a51c20a33cca8a3936afdac9ab0dc449c9407b12
Chibueze Nelson Ayogu <chibuezeayogu@hotmail.com>
2021-03-19 19:52:58 +0100

    Minor code refactor (#5)

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Product } from "../../types";
 import { CURRENCY } from "../../util/index";
+import { useQuery } from "@apollo/client";
+import { LOAD_CURRENCY } from "./../../graphQL/Queries";
 
 const CartItem = (
   {
@@ -11,26 +13,28 @@ const CartItem = (
   currency
   }:{ 
   cart: Product; 
-  incrementQuantity: MouseEventHandler<HTMLButtonElement>; 
-  decrementQuantity: MouseEventHandler<HTMLButtonElement>; 
-  removeFromCart: MouseEventHandler<HTMLButtonElement>;
+  incrementQuantity: (id: number) => void; 
+  decrementQuantity: (id: number, quantity: number) => void; 
+  removeFromCart: (id: number) => void; 
   currency: string  
-}): JSX.Element => (
-  <div className="cart-item">
-    <div className="cart-item__title">
-      <span>{cart.title}</span>
-      <button onClick={()=> removeFromCart(cart)} className="close">{"X"}</button>
-    </div>
-    <div className="cart-item__body">
-      <div className="quantity-btn">
-        <button onClick={()=> decrementQuantity(cart)}>-</button>
-        <span>{cart.quantity}</span>
-        <button onClick={() => incrementQuantity(cart)}>+</button>
+}): JSX.Element => {
+  return (
+    <div className="cart-item">
+      <div className="cart-item__title">
+        <span>{cart.title}</span>
+        <button onClick={()=> removeFromCart(cart.id)} className="close">{"X"}</button>
       </div>
-      <span>{CURRENCY[currency]}{cart.quantity * cart.price}</span>
-      <span><img src={cart.image_url} alt={cart.title}/></span>
+      <div className="cart-item__body">
+        <div className="quantity-btn">
+          <button onClick={()=> decrementQuantity(cart.id, cart.quantity)}>-</button>
+          <span>{cart.quantity}</span>
+          <button onClick={() => incrementQuantity(cart.id)}>+</button>
+        </div>
+        <span>{CURRENCY[currency]}{ cart.price} {cart.price}</span>
+        <span><img src={cart.image_url} alt={cart.title}/></span>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default CartItem;
